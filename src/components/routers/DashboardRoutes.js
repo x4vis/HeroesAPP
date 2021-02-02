1d59492f087f09351bd630683f3417eb7cf5d179
Javier Soto <javislop@gmail.com>
2021-02-02 00:19:55 -0600

    add classes on dashboard routes

--- src/components/routers/DashboardRoutes.js
+++ src/components/routers/DashboardRoutes.js
@@ -12,7 +12,7 @@ export const DashboardRoutes = () => {
     <>
       <Navbar />
 
-      <div>
+      <div className="container mt-2">
         <Switch>
           <Route exact path="/marvel" component={MarvelScreen} />
           <Route exact path="/heroe/:heroeId" component={HeroScreen} />
