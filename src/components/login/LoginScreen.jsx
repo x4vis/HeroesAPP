083f140d0f94ddbf7044c12741dec34935854328
Javier Soto <javislop@gmail.com>
2021-02-01 23:56:39 -0600

    add dashboard routes to the router, modify the login screen, to do some redirections with history from react router dom

--- src/components/login/LoginScreen.jsx
+++ src/components/login/LoginScreen.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({history}) => {
+
+    const handleClick = ()=> {
+        //Con el push nos dirigimos hacia la ruta que le pasamos al metodo
+        //con la opcion de poder dar en la flecha atras del navegador y que
+        //nos regrese a la pantalla anterior
+        
+        //history.push('/');
+        
+        //Con el replace, la diferencia es que al darle en la flecha hacia
+        //atras del navegador no podremos regresar a la pantalla desde donde 
+        //hicimos la redireccion
+        history.replace('/');
+    }
+
     return (
-        <div>
+        <div className="container mt-5">
             <h1>Login Screen</h1>
+
+            <hr/>
+
+            <button className="btn btn-primary"
+                    onClick={handleClick}>
+                Login
+            </button>
         </div>
     )
 }
---
+++ src/components/routers/DashboardRoutes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import { Navbar } from '../ui/Navbar';
+import { HeroScreen } from '../heroes/HeroScreen';
+import { MarvelScreen } from '../marvel/MarvelScreen';
+import { DcScreen } from '../dc/DcScreen';
+
+
+export const DashboardRoutes = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div>
+        <Switch>
+          <Route exact path="/marvel" component={MarvelScreen} />
+          <Route exact path="/heroe/:heroeId" component={HeroScreen} />
+          <Route exact path="/dc" component={DcScreen} />
+
+          <Redirect to="/marvel" />
+        </Switch>
+      </div>
+    </>
+  )
+}
