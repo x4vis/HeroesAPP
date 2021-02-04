755adf24fc25cf19941dbdd63fa2199edc3787bf
Javier Soto <javislop@gmail.com>
2021-02-03 23:12:55 -0600

    rework navbar and do herocard

--- src/components/heroes/HeroList.jsx
+++ src/components/heroes/HeroList.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({publisher}) => {
 
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
+    <div className="row row-cols-2 row-cols-xl-3 g-4">
       {
         heroes.map(hero => (
-          <li key={hero.id}>
-            { hero.superhero }
-          </li>
+          <HeroCard key={hero.id} 
+                    {...hero} />
         ))
       }
-    </ul>
+    </div>
   )
 }
 
