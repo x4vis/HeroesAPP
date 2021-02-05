14c8359ee95b5463b5e91532f7fbfcf6cc22d2a4
Javier Soto <javislop@gmail.com>
2021-02-05 13:44:26 -0600

    add animations

--- src/components/heroes/HeroList.jsx
+++ src/components/heroes/HeroList.jsx
@@ -8,7 +8,7 @@ export const HeroList = ({publisher}) => {
   const heroes = useMemo (() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
-    <div className="row row-cols-2 row-cols-xl-3 g-4">
+    <div className="row row-cols-2 row-cols-xl-3 g-4 animate__animated animate__fadeIn">
       {
         heroes.map(hero => (
           <HeroCard key={hero.id} 
--- src/components/heroes/HeroScreen.jsx
+++ src/components/heroes/HeroScreen.jsx
@@ -34,12 +34,12 @@ export const HeroScreen = ({history}) => {
 	return (
 		<div className="row mt-5">
 			<div className="col-4">
-				<img className="img-thumbnail" 
+				<img className="img-thumbnail animate__animated animate__fadeInLeft" 
 						 src={`../assets/heroes/${heroId}.jpg`} 
 						 alt={superhero}/>
 			</div>
 
-			<div className="col-8">
+			<div className="col-8 animate__animated animate__fadeIn">
 				<h3>{superhero}</h3>
 				<ul className="list-group list-group-flush">
 					<li className="list-group-item">
