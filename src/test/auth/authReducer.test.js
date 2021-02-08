48c7083ffc4ccd50494529ec05e3821603422608
Javier Soto <javislop@gmail.com>
2021-02-07 22:57:18 -0600

    add tests to authReducer

---
+++ src/test/auth/authReducer.test.js
@@ -0,0 +1,36 @@
+import { authReducer } from "../../auth/authReducer";
+import { types } from "../../types/types";
+
+describe('Pruebas en authReducer', () => {
+
+    test('debe de retornar el estado por defecto', () => {
+      const state = authReducer({ logged: false }, {});
+      expect( state ).toEqual({ logged: false });
+    });
+
+    test('debe de autenticar y colocar el name del usuario', () => {
+      const action = {
+          type: types.login,
+          payload: {
+            name: 'Javier'
+          }
+      };
+
+      const state = authReducer({ logged: false }, action);
+      expect(state).toEqual({
+          logged: true,
+          name: 'Javier'
+      });
+    });
+
+    test('debe de borra el nombre del usuario y el logged en false', () => {
+        const action = {
+            type: types.logout
+        }
+
+        const state = authReducer({ logged: true, name: 'Javier' }, action);
+        expect(state).toEqual({ logged: false });
+    });
+    
+})
+  
